4b833c0fe3ae93f0a0c3564e9ac1e329b8ea7518
siak631 <143899048+siak631@users.noreply.github.com>
2023-12-21 20:25:54 +0100

    bot-notif(2)

---
+++ tests.py
@@ -0,0 +1,14 @@
+# pour exploiter l'API discord jai commencé par :
+import discord
+# pour choisir une valeur au hasard a la generation de la notif jai fait un  :
+import random 
+# ensuite, pour effectuer des entrées et sorties reseaux pour le bon fonctionnement du bot, j'ai executé un :
+import asyncio 
+# pour pouvoir lire un chemin de ficher pour le bot : 
+import os 
+# pour manipuler des valeurs de temps, en lien avec le import random, jai fait un : 
+from asyncio import sleep
+# toujours en lien avec l'importation de nombre, jai execute cela pour generer un nombre secret dans l'interval [0;n] (on sait jamais si jen ai besoin !!!)
+from secrets import randbelow
+# et enfin 
+import logging
